ffffd46c1786f0ceb10843f8a2c294894ad42684
Justin Bell <justinbell0114@gmail.com>
2026-08-23 15:36:39 -0400

    added fixedpoint adder and subtractor

--- src/fixed_point_add.sv
+++ src/fixed_point_add.sv
@@ -2,9 +2,9 @@ module fp_add
 #(paramter FRACTION_BITS = 8,
   parameter WHOLE_BITS = 8
 )
-(input logic  signed [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] a,
- input logic  signed  [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] b,
- output logic signed [(FRACTION_BITS-1)+(WHOLE_BITS-1)+1:0] out_x);
+(input logic  signed [15:0] a,
+ input logic  signed  [15:0] b,
+ output logic signed [16:0] out_x);
 
     assign out_x = a + b;
  endmodule;

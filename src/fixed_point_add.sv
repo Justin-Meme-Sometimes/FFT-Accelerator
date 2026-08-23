fe68149d982ec9a9491ed66d955a8332c6a53735
Justin Bell <justinbell0114@gmail.com>
2026-08-22 23:30:23 -0400

    added fft add

--- src/fixed_point_add.sv
+++ src/fixed_point_add.sv
@@ -2,9 +2,9 @@ module fp_add
 #(paramter FRACTION_BITS = 8,
   parameter WHOLE_BITS = 8
 )
-(input logic [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] a,
- input logic [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] b,
- output logic [(FRACTION_BITS-1)+(WHOLE_BITS-1)+1:0] out_x);
+(input logic  signed [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] a,
+ input logic  signed  [(FRACTION_BITS-1)+(WHOLE_BITS-1):0] b,
+ output logic signed [(FRACTION_BITS-1)+(WHOLE_BITS-1)+1:0] out_x);
 
     assign out_x = a + b;
  endmodule;
